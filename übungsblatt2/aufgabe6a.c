e92ff9e1edf10124e4d6d31868c0e0ed92419a9a
Felix Weilbach <feweilbach@gmail.com>
2017-11-04 17:22:45 +0100

    update

--- übungsblatt2/aufgabe6a.c
+++ übungsblatt2/aufgabe6a.c
@@ -8,6 +8,12 @@ int main()
     scanf("%c%c", &x, &y);
     res = isdigit(x);
     res1 = isdigit(y);
+
+    /*
+      Funktioniert zwar aber es ist nicht festgelegt das isdigit() bei Erfolg
+      immer 2048 zurück gibt.
+      Das Programm ist also zum scheitern verurteilt.
+    */
     printf("%d\n", (res * res1) / (2048 * 2048));
     return 0;
 }
